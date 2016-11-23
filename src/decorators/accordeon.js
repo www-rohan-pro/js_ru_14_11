e86ef4829017dccb7b0266281f1d4cb6a8b5d2e5
Roman <kaboom.ua@gmail.com>
2016-11-23 21:13:16 +0200

    Update accordeon.js

--- src/decorators/accordeon.js
+++ src/decorators/accordeon.js
@@ -4,6 +4,7 @@ export default (Component) => class WrappedComponent extends React.Component {
     constructor() {
         super()
         this.state = {
+            //не привязывайся к названиям сущностей в декораторах, вся их суть в том, чтобы использовать с разными компонентами и данными. Сделай openItemId
             openArticleId: null
         }
     }
